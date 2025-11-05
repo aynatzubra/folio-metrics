b4ad90b825ab8f73d536df146c468cd0bcd28404
arbuz.tp <arbuz.tp@gmail.com>
2025-11-05 12:34:59 +0200

    refactor(i18n)!: remove ro locale and update other translations

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
-export const locales = ['en', 'ro', 'ru'] as const
+export const locales = ['en', 'ru'] as const
 export type AppLocale = (typeof locales)[number]
 
 export const defaultLocale: AppLocale = 'en'
